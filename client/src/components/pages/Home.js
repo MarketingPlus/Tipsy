d84bc0c5527bb0bb2da39f001cee67840de0eaa1
MarketingPlus <benmarketingplus@gmail.com>
2021-07-31 19:13:02 +1000

    added some const including user which will autheticate users, months for the seasons, drinksearch, drinkresults and drink list which will all display drinks or recipes respectedly

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -28,3 +28,17 @@ const styles = {
       fontSize: 34
     }
 }
+
+function Home() {
+    const user = useAuthenticatedUser();
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"
+    ];
+    const d = new Date();
+    const defaultMonth = monthNames[d.getMonth()];
+    console.log(defaultMonth);
+  
+    const [drinkSearch, setdrinkSearch] = useState("");
+    const [drinkResult, setdrinkResult] = useState({});
+    const [month, setMonth] = useState(defaultMonth);
+    const [drinkList, setdrinkList] = useState(Drink);

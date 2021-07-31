9aaf73be263c7a6784ee4409caa0c5bfed16b182
MarketingPlus <benmarketingplus@gmail.com>
2021-07-31 19:09:09 +1000

    added the React dom render code to ensure everything gets rendered on the page correctly

--- client/src/index.js
+++ client/src/index.js
@@ -4,3 +4,13 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import { StoreProvider } from "./store";
+
+ReactDOM.render(
+    <React.StrictMode>
+  
+      <StoreProvider>
+          <App />
+      </StoreProvider>
+    </React.StrictMode>,
+      document.getElementById('root')
+  );

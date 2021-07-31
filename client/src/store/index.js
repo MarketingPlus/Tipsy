aafbf9160e9881e09ea813c2f5ad85b9d9548e91
MarketingPlus <benmarketingplus@gmail.com>
2021-07-31 20:12:42 +1000

    added a reducer to make sure it autheticates the user to make sure the information is stored to the correct user and also they are actually logged in

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -11,3 +11,30 @@ const StoreContext = createContext({
     userAuth: {},
     recipe: null
 });
+
+const { Provider } = StoreContext;
+
+const reducer = ( state, { type, payload } ) => {
+
+    switch( type ) {
+        case SET_FAVORITES:
+
+            return { ...state, userAuth: { ...state.userAuth, user: { ...state.userAuth.user, favorites: payload } }}
+
+        case LOGIN_USER:
+
+            return { ...state, userAuth: payload };
+
+        case LOGOUT_USER:
+
+            return { ...state, userAuth: {} };
+
+        case SET_CURRENT_RECIPE:
+            console.log("setting recipe", payload);
+            return { ...state, recipe: payload }
+
+        default:
+            return state;
+    }
+
+}

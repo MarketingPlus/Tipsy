09ac6f49fc0318038e65c8a41c969b5d33a4c767
MarketingPlus <benmarketingplus@gmail.com>
2021-07-31 19:06:05 +1000

    added my components to my app.js file

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+import Home from './components/pages/Home';
+import Account from './components/pages/Account';
+import Recipe from './components/pages/Recipe'
+import Hero from './components/Hero';
+import Logo from './components/Logo';
+import Login from './components/pages/Login'
+import GuestRoute from './components/GuestRoute'
+import PrivateRoute from "./components/PrivateRoute";

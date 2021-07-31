e3c866e3ba49aa2e9d035a4d94f40cd2df8d5740
MarketingPlus <benmarketingplus@gmail.com>
2021-07-31 20:07:19 +1000

    added stuff to the recipe cards including the style for the card

---
+++ client/src/components/RecipeCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+const styles = {
+  heart: {
+    maxWidth: 13,
+    maxHeight: "auto"
+  },
+  badge: {
+    marginLeft: 5
+    },
+    button: {
+        backgroundColor: "#2C95E1",
+        border: "none"
+    },
+    header: {
+        color: "#2F3439"
+    },
+    expand: {
+        maxWidth: 11,
+        maxHeight: "auto",
+        marginLeft: 10,
+        verticalAlign: "baseline"
+        },
+};
+

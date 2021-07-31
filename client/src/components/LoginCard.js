fdb7dac8c0467500eda78a726a80a229334c0013
MarketingPlus <benmarketingplus@gmail.com>
2021-07-31 20:03:14 +1000

    added a login card return so it properly displayes on the page

--- client/src/components/LoginCard.js
+++ client/src/components/LoginCard.js
@@ -67,3 +67,52 @@ const LoginCard = () => {
       const id = evt.target.id
       setsignUpForm(id === 'btn-signup' ? false : true)
     }, [])
+
+    return (
+
+        <div id="logreg-forms">
+          {
+            signUpForm
+              ? (
+                <form className="form-signin" onSubmit={handleLoginSubmit} data-component="Form-Signin">
+                  <h1 className="h3 mb-3 font-weight-normal" style={{ textAlign: 'center' }}> Sign in </h1>
+                  {/* <div className="social-login">
+                    <button className="btn facebook-btn social-btn text-center" type="button"><span><i className="fab fa-facebook-f" /> Sign in with Facebook </span></button>
+                    <button className="btn google-btn social-btn text-center" type="button"><span><i className="fab fa-google" /> Sign in with Google </span></button>
+                  </div> */}
+                  {/* <p style={{ textAlign: 'center' }}>OR</p> */}
+                  <input type="email" onChange={(e) => setEmail(e.target.value)} id="inputEmail" className="form-control" placeholder="Email address" required autoFocus />
+                  <input type="password" onChange={(e) => setPassword(e.target.value)} id="inputPassword" className="form-control" placeholder="Password" required />
+                  <button className="btn btn-success btn-block" style={styles.button} onSubmit={handleLoginSubmit} type="submit"><i className="fas fa-sign-in-alt"  style={styles.i}/> Sign in </button>
+                  <hr />
+                  <button className="btn btn-primary btn-block" type="button" style={styles.button2} id="btn-signup" onClick={onFormChange}>
+                    <i className="fas fa-user-plus" style={styles.i}/> Sign up with New Account
+                      </button>
+                </form>
+              )
+              : (
+                <form action="/signup/" onSubmit={handleSignUpSubmit} className="form-signin">
+                  {/* <div className="social-login" style={{ textAlign: 'center' }}>
+                    <button className="btn facebook-btn social-btn" type="button"><span><i className="fab fa-facebook-f" /> Sign up with Facebook </span></button>
+                  </div>
+                  <div className="social-login" style={{ textAlign: 'center' }}>
+                    <button className="btn google-btn social-btn" type="button"><span><i className="fab fa-google" /> Sign up with Google </span></button>
+                  </div>
+                  <p style={{ textAlign: 'center' }}>OR</p> */}
+                  <input type="text" onChange={(e) => setName(e.target.value)} id="user-name" className="form-control" placeholder="Full name" required autoFocus />
+                  <input type="email" onChange={(e) => setNewEmail(e.target.value)} id="user-email" className="form-control" placeholder="Email address" required autoFocus />
+                  <input type="password" onChange={(e) => setNewPassword(e.target.value)} id="user-pass" className="form-control" placeholder="Password" required autoFocus />
+                  <input type="password" onChange={(e) => verifyNewPassword(e.target.value)} id="user-repeatpass" className="form-control" placeholder="Confirm Password" required autoFocus />
+                  <button className="btn btn-primary btn-block" type="submit" style={styles.button} ><i className="fas fa-user-plus"  style={styles.i} /> Sign Up </button><br/>
+                  <button id="btn-back btn" className="fas fa-angle-left btn-sm" type="button" onClick={onFormChange} style={styles.button2}></button>
+                </form>
+              )
+          }
+          {/* <br />
+                  <p style={{textAlign: 'center'}} /> */}
+        </div>
+      )
+    }
+    
+    export default LoginCard;
+    

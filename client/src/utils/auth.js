6ab618c32265535c0caabf6d7508fc5ef7dc30e5
MarketingPlus <benmarketingplus@gmail.com>
2021-07-31 20:14:34 +1000

    added some authtoekn which will apply a token, store a token and set the auth token

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -4,3 +4,34 @@ import jwt_decode from "jwt-decode";
 import api from "./register-api";
 import { useStoreContext } from "../store";
 import { LOGIN_USER, LOGOUT_USER } from "../store/actions";
+
+const setAuthToken = token => {
+
+    storeAuthToken( token );
+    applyAuthToken( token );
+
+    return token ? jwt_decode(token) : undefined;
+
+}
+
+const storeAuthToken = token => {
+
+    token
+
+        ? localStorage.setItem("jwtToken", token)
+        
+        : localStorage.removeItem( "jwtToken" );
+
+}
+
+const applyAuthToken = token => {
+
+    token
+
+        // Apply authorization token to every request if logged in
+        ? api.setHeader( "Authorization", token )
+
+        // Delete auth header
+        : api.setHeader( "Authorization", false );
+
+}

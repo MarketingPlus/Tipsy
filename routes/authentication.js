5e5996d43ff8d3267709816a5ae7ca2a980b02b3
MarketingPlus <benmarketingplus@gmail.com>
2021-07-31 20:47:49 +1000

    added the data validators and the user model while also including the jwt signing which will currenlt autheticate the user

--- routes/authentication.js
+++ routes/authentication.js
@@ -9,3 +9,18 @@ const passwordHash = require("../config/passwordHash");
 // Get middleware
 const authenticateUser = require("./middleware/authenticateUser");
 const validateBodyWith = require("./middleware/validateBodyWith");
+
+// Data validators
+const { loginValidator, registerValidator } = require("./validation");
+
+// Load User model
+const { User } = require("../models");
+
+const jwtSign = util.promisify( jwt.sign );
+
+// Get the currently authenticated user
+router.post("/user/authenticated", authenticateUser, (req, res) => {
+
+  res.json( req.user );
+
+});

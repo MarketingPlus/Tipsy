843bfc5c58a06533dc6557fdffb30c0548de75a6
MarketingPlus <benmarketingplus@gmail.com>
2021-07-31 19:07:18 +1000

    added my authetic token to ensure everyone is authetnicated

--- client/src/App.js
+++ client/src/App.js
@@ -10,3 +10,11 @@ import Logo from './components/Logo';
 import Login from './components/pages/Login'
 import GuestRoute from './components/GuestRoute'
 import PrivateRoute from "./components/PrivateRoute";
+// Import the useAuthTokenStore hook.
+import { useAuthTokenStore, useIsAuthenticated } from "./utils/auth";
+
+const App = () => {
+
+  const isAuthenticated = useIsAuthenticated();
+  // Use the hook to reauthenticate stored tokens.
+  useAuthTokenStore();

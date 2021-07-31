915765378f01bb1c119f7e72427c77b7dee12bbe
MarketingPlus <benmarketingplus@gmail.com>
2021-07-31 16:56:21 +1000

    imported all my required files for my account.js

---
+++ client/src/components/pages/Account.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import FavoriteDrink from '../FavoriteDrink';
+import FavoriteRecipes from "../FavoriteRecipes";
+import Card from '../Card';
+import Drink from '../Drink.json';
+import { useAuthenticatedUser } from '../../utils/auth';
+import {useLogout} from "../../utils/auth";
+
+export default Account;

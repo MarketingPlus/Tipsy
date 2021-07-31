e993682adeee5ed10739567238d0b9c973fb972d
MarketingPlus <benmarketingplus@gmail.com>
2021-07-31 19:56:01 +1000

    added the favourite drinsk componenet

---
+++ client/src/components/FavoriteDrink.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//maybe use usestate;
+
+const FavoriteDrink = () => {
+      return (
+          <div className="DrinkBox">
+                <h1 style={{textAlign: 'left'}}>Favorites Drink</h1>   
+          </div>
+   
+
+        
+      );
+
+}
+export default FavoriteDrink;

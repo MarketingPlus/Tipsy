f48800be3231707214b7723cdfde0639eb56ca4f
MarketingPlus <benmarketingplus@gmail.com>
2021-07-31 20:50:21 +1000

    defined middleware

--- server.js
+++ server.js
@@ -7,3 +7,14 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(passport.initialize());
+
+app.use("/api",require("./routes/authentication"));
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}

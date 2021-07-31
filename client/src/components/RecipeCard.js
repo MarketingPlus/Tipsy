3d7bcf8d2cb8f67e0bee9a6b3cff4ad7fb80b683
MarketingPlus <benmarketingplus@gmail.com>
2021-07-31 20:10:21 +1000

    added a recipt card including all the information such as the ingrediences and how to make them with a link to the new page where the user can find more information about the recipe

--- client/src/components/RecipeCard.js
+++ client/src/components/RecipeCard.js
@@ -24,3 +24,69 @@ const styles = {
         },
 };
 
+const RecipeCard = ({ recipe }) => {
+
+    return (
+      recipe.map(hit => {
+        return (
+          <div className="col mb-4">
+  
+  
+            <div className="card h-100 text-center">
+  
+  
+              <img
+                src={hit.recipe.image}
+                className="card-img-top img-fluid"
+                alt={hit.recipe.label} />
+              <div className="card-body">
+                {/* <span>
+                  <a href="#"
+                    className="badge badge-danger float-right" style={styles.badge}>
+                    <img src={process.env.PUBLIC_URL + "/assets/heart.png"} style={styles.heart} />
+                  </a>
+                </span> */}
+  
+                {
+                  // }<h5 className="card-title">{foodResult.food? foodResult.food.label:("")}</h5>
+                }<h6 className="card-title" style={styles.header}>
+                  {/* <a rel="noreferrer noopener"href={"https://en.wikipedia.org/w/api.php?action=opensearch&search="+props.title+"&limit=1&format=json&origin=*"}>{props.title}</a> */}
+                  {hit.recipe.label}
+                </h6>
+  
+                <p className="card-text">
+  
+                </p>
+                <div style={styles.option}>
+                  <p className="card-text" style={styles.option}>
+                    <small className="text-muted text-left" style={styles.option}>
+                      {hit.recipe.ingredientLines.map(ingredient => {
+                        return <li>{ingredient}</li>
+                      })}
+  
+                    </small></p></div><br />
+                <div className="card-body">
+                  <a href={hit.recipe.url} className="card-link btn btn-sm btn-secondary" style={styles.button}>
+                                View Full Recipe
+                                             
+                                <span>
+                            <img
+                                src={process.env.PUBLIC_URL + '/assets/resize.png'}
+                                alt='Open in new tab'
+                                style={styles.expand}/>
+                        </span></a>
+                                            
+  
+  
+                </div>
+  
+              </div>
+            </div>
+          </div>
+        )
+  
+      })
+  
+    )
+  };
+  export default RecipeCard;

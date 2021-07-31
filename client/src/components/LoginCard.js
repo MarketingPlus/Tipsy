af66cf7324c95e19a61eb2c78b67307d960b60e9
MarketingPlus <benmarketingplus@gmail.com>
2021-07-31 20:02:34 +1000

    added a sumbit handler, signup form switcher, and verify password controller

--- client/src/components/LoginCard.js
+++ client/src/components/LoginCard.js
@@ -20,3 +20,50 @@ const styles = {
     paddingRight: 5
   }
 }
+
+const LoginCard = () => {
+    // Gets old user info
+    const [inputEmail, setEmail] = useState("");
+    const [inputPassword, setPassword] = useState("");
+    // console.log(inputEmail, inputPassword);
+    const login = useLogin();
+    // Saves new user info
+    const [signupEmail, setNewEmail] = useState("");
+    const [signupName, setName] = useState("");
+    const [signupPassword, setNewPassword] = useState("");
+    const [verifyPassword, verifyNewPassword] = useState("");
+    // console.log(signupName, signupEmail, signupPassword, verifyPassword);
+    // Tries to use login component
+  
+    // Handles error
+    const handleLoginSubmit = async e => {
+      e.preventDefault();
+  
+      try {
+        const email = inputEmail;
+        const password = inputPassword;
+        await login({email,password});
+      } catch (err) {
+        console.log(err)
+      }
+    };
+  
+    const handleSignUpSubmit = async e => {
+      e.preventDefault();
+      const email = signupEmail;
+      const password = signupPassword;
+      try {
+        await api.register({ email, password });
+        await login({ email, password });
+      } catch (err) {
+        console.log(err)
+      }
+    };
+  
+  
+    // Switches between Login/Signup --> Should be broken into 2 components
+    const [signUpForm, setsignUpForm] = React.useState(true)
+    const onFormChange = React.useCallback((evt) => {
+      const id = evt.target.id
+      setsignUpForm(id === 'btn-signup' ? false : true)
+    }, [])

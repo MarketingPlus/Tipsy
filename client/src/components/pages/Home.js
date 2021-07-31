b96532aec0407e117244950cc03d90d8ddeaccfd
MarketingPlus <benmarketingplus@gmail.com>
2021-07-31 19:11:39 +1000

    added some styling to my home component

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -6,3 +6,25 @@ import ItemOption from '../ItemOption';
 import calendar from '../../assets/calendar.png';
 import RapidApi from "../../utils/RapidApi";
 import {useAuthenticatedUser, useIsAuthenticated} from '../../utils/auth';
+
+const styles = {
+    search: {
+        marginBottom: 25,
+        backgroundColor: "#2C95E1",
+        color: "white"
+    },
+    icon: {
+        maxWidth: 15,
+        maxHeight: "auto"
+    },
+    hr: {
+        paddingBottom: 10
+    },
+    search2: {
+        backgroundColor: "#2C95E1",
+        color: "white",
+  },
+  head: {
+      fontSize: 34
+    }
+}

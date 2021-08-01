2e5e24e837228ea68a55a95c042354b941592f31
MarketingPlus <benmarketingplus@gmail.com>
2021-08-01 19:28:17 +1000

    added some more files

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import logo from '../assets/tipsy-logo-white.svg';
 import Home from '../components/pages/Home'
+import LogoutButton from './LogoutButton';
 
 const styles = {
     nav: {
--- client/src/utils/recipeApi.js
+++ client/src/utils/recipeApi.js
@@ -3,6 +3,7 @@ export default {
 
     getRecipes: function (query) {
         return fetch(`https://api.edamam.com/search?q=${query}&app_id=${env.RECIPE_ID}&app_key=${env.RECIPE_KEY}&from=0&to=50&dishType=Alcohol&imageSize=REGULAR`,{
+        // return fetch(`https://api.edamam.com/search?q=${query}&app_id=b6ab5a66&app_key=20b5c8c234f54b26b93a27807b0390b1&health=vegetarian`,{
           "method":"GET"
         })
         .then(response => {
@@ -17,4 +18,7 @@ export default {
           console.error(err);
         })
       }
-};
+    };
+
+
+// return axios.get(`https://api.edamam.com/search?q=${query}&app_id=${process.env.RECIPE_ID}&app_key=${process.env.RECIPE_KEY}&health=vegetarian`)

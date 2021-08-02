7299cb4f52dda3e224079c06f822226867dd89b8
MarketingPlus <benmarketingplus@gmail.com>
2021-08-02 21:35:08 +1000

    changed url link

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,12 @@
 const router = require("express").Router();
 
 const userRoutes = require("./user");
+const authRoutes = require("./authentication");
 
-router.use("/user",userRoutes);
+router.use("/user", userRoutes);
 
+//! Make sure to use the route as `/auth` to avoid collision,
+//! since you have defined `/user/login` in the authentication module.
+router.use("/auth", authRoutes);
 
 module.exports = router;

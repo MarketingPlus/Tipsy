d0fcfb7242c985d60ed38a3dbce998288d46cd35
MarketingPlus <benmarketingplus@gmail.com>
2021-08-02 20:54:31 +1000

    changed route

--- client/src/utils/register-api.js
+++ client/src/utils/register-api.js
@@ -38,7 +38,7 @@ class API {
       console.log(userData);
       console.log("------");
 
-        return this.axios.post("/api/auth/user/register", userData);
+        return this.axios.post("/api/user/register", userData);
 
     }
 
@@ -52,13 +52,13 @@ class API {
      */
     login( userData ) {
 
-        return this.axios.post("/api/auth/user/login", userData);
+        return this.axios.post("/api/user/login", userData);
 
     }
 
     authenticated() {
 
-        return this.axios.post("/api/auth/user/authenticated");
+        return this.axios.post("/api/user/authenticated");
 
     }
 

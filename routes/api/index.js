d2be58082959aae8a95fae2e6e04b6b924db57a8
MarketingPlus <benmarketingplus@gmail.com>
2021-08-02 15:49:58 +1000

    changing some files to see if my routes a fixed properly

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,9 +3,10 @@ const router = require("express").Router();
 const userRoutes = require("./user");
 const authRoutes = require("./authentication");
 
-router.use("/user",userRoutes);
+router.use("/user", userRoutes);
 
+//! Make sure to use the route as `/auth` to avoid collision,
+//! since you have defined `/user/login` in the authentication module.
 router.use("/auth", authRoutes);
-
 
 module.exports = router;

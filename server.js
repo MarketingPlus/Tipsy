6b8dc2a9e817c29b995604c2ead23fc07e4e75d5
MarketingPlus <benmarketingplus@gmail.com>
2021-08-02 20:32:16 +1000

    added auth to my request url

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(passport.initialize());
 
-app.use("/api",require("./routes/authentication"));
+app.use("/api",require("./routes/api/authentication"));
 app.use("/auth",validatorRoutes)
 app.use("/api", apiRoutes);
 

6be95c914a4c9081813696cf15725220bde137e1
MarketingPlus <benmarketingplus@gmail.com>
2021-08-02 14:41:55 +1000

    changed some files around

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
 
 const userRoutes = require("./user");
+const authRoutes = require("./authentication");
 
 router.use("/user",userRoutes);
+
+router.use("/auth", authRoutes);
 
 
 module.exports = router;

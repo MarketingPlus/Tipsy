d5c87649c744466c4233e525795784ce06826655
MarketingPlus <benmarketingplus@gmail.com>
2021-08-03 19:07:27 +1000

    removed code from register api

--- client/src/utils/register-api.js
+++ client/src/utils/register-api.js
@@ -38,7 +38,7 @@ class API {
       console.log(userData);
       console.log("------");
 
-        return this.axios.post("/api/user/register", userData);
+        return this.axios.post("/api/register", userData);
 
     }
 
@@ -52,13 +52,13 @@ class API {
      */
     login( userData ) {
 
-        return this.axios.post("/api/user/login", userData);
+        return this.axios.post("/api/login", userData);
 
     }
 
     authenticated() {
 
-        return this.axios.post("/api/user/authenticated");
+        return this.axios.post("/api/authenticated");
 
     }
 

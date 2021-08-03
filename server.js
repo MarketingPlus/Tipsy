b023c31502b3fcd438df67124139455c640bc514
MarketingPlus <benmarketingplus@gmail.com>
2021-08-03 20:51:57 +1000

    changed some things

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/ripe",
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/tipsy",
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
